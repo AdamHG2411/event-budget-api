78c84c1de6980e1d67c7d23ef9a17d59b9cbc76b
Adam Grider <adam.h.grider@gmail.com>
2019-05-20 11:15:48 -0400

    change model structure and seed file

--- db/seed.js
+++ db/seed.js
@@ -1,27 +1,9 @@
-const mongoose = require('./connection.js');
+const User = require('./models/User.js');
+const data = require('./data.json');
 
-const Event = require('../db/models/Event.js');
-const Expense = require('../db/models/Expense.js');
-const evtData = require('./evtData.json');
-
-//Delete all entries
-Event.deleteMany({}).then(() => {
-	console.log('deleted all events');
-	Expense.deleteMany({}).then(() => {
-		console.log('deleted all expenses');
-		//Seed events collection
-		Event.create(evtData).then((events) => {
-			//Seed expenses collection
-			Expense.create(evtData).then(() => {
-				//Find the expenses associated with each event and push them to the event's expenses array
-				events.forEach((event) => {
-					Expense.find({ evtCode: event.code }).then((expenses) => {
-						expenses.forEach((expense) => {
-							event.expenses.push(expense._id);
-						});
-					});
-				});
-			});
-		});
+User.deleteMany({}).then(() => {
+	console.log('deleted all users');
+	User.create(data).then((users) => {
+		console.log(users);
 	});
 });
