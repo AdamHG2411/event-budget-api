4300371d578b6f2a8d90bc702c39a7bf61a02ed6
Adam Grider <adam.h.grider@gmail.com>
2019-05-23 14:12:48 -0400

    fix delete user

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -22,30 +22,8 @@ module.exports = {
 		});
 	},
 	delete: (req, res) => {
-		User.findOne({ _id: req.params.userId }).then((user) => {
-			if (user.events) {
-				Event.find({ userId: req.params.userId }).then((events) => {
-					events.forEach((thisEvent) => {
-						console.log(thisEvent);
-						if (thisEvent.expenses) {
-							Expense.deleteMany({ eventId: event._id }).then((delExp) => {
-								console.log(delExp);
-							});
-						}
-						Event.deleteOne({ _id: event._id }).then((delEvent) => {
-							console.log(delEvent);
-						});
-					});
-				});
-			}
-			if (user.bills) {
-				Bill.deleteMany({ userId: req.params.userId }).then((delBills) => {
-					console.log(delBills);
-				});
-			}
-			User.deleteOne({ _id: req.params.userId }).then((delUser) => {
-				res.json(delUser);
-			});
+		User.deleteOne({ _id: req.params.userId }).then((user) => {
+			res.json(user);
 		});
 	}
 };
